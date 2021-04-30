93b8bcf9a7631a0651a7c3acec5122bb3ad0c839
unknown <vivekrajsingh007@gmail.com>
2021-04-30 16:19:37 +0530

    data types

---
+++ app/src/main/java/com/demo/kotlindemo/DataType.kt
@@ -0,0 +1,37 @@
+package com.demo.kotlindemo
+
+class DataType {
+
+    fun issues(){
+        val a: Int = 100
+        val boxedA: Int? = a
+        val anotherBoxedA: Int? = a
+
+        val b: Int = 10000
+        val boxedB: Int? = b
+        val anotherBoxedB: Int? = b
+
+        println(boxedA === anotherBoxedA) // true
+        println(boxedB === anotherBoxedB) // false
+    }
+
+    fun expilicityConversion(b:Byte,float: Float){
+
+        println(b.toShort())
+
+        println(float.toDouble())
+        println(float.toLong())
+
+
+    }
+
+
+}
+
+var i=5  //by default its takes int value. Int 32 bites
+val l:Long=1000000000L //long 64 bites
+val b:Byte=25  /// 8 bites
+val s:Short=6666 //16 bites
+
+val d=3.8887 // by default its take double. 64 bite
+val f:Float=6.6666F // float 32 bites
---
+++ app/src/main/java/com/demo/kotlindemo/MainDemo.kt
@@ -0,0 +1,22 @@
+package com.demo.kotlindemo
+
+fun main(){
+
+    val dataType=DataType()
+    dataType.issues()
+    dataType.expilicityConversion(22,5.69999f)
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
